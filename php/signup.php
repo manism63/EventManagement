0cdb8897caedaaa8a4c34ff32920305df2419d19
anuragg2301 <anurag.jain.2301@gmail.com>
2018-04-17 20:22:57 -0400

    asd

--- php/signup.php
+++ php/signup.php
@@ -8,6 +8,7 @@ $dbname = "eventmanagement";
 $conn = new mysqli($servername, $username, $password, $dbname);
 // Check connection
 if ($conn->connect_error) {
+	echo "Connection failed";
     die("Connection failed: " . $conn->connect_error);
 } 
 echo "Connection successful";
