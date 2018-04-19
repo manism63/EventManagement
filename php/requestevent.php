bbe586709d2805af858ea87d234fc65300f71aa5
anuragg2301 <anurag.jain.2301@gmail.com>
2018-04-19 18:30:08 -0400

    Updates

---
+++ php/requestevent.php
@@ -0,0 +1,26 @@
+<?php
+// $servername = "149.160.213.45";
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "eventmanagement";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+	echo "Connection failed";
+    die("Connection failed: " . $conn->connect_error);
+}
+echo "Connection successful";
+$sql = "INSERT INTO request_event(email_id, request_desc) VALUES('" . $_SESSION['email_id'] . '\', \'' . $_POST['desc'] . '\')';
+
+echo $sql;
+if ($conn->query($sql) === TRUE) {
+    echo "New record created successfully";
+} else {
+    echo "Error: " . $sql . "<br>" . $conn->error;
+}
+
+$conn->close();
+?>
